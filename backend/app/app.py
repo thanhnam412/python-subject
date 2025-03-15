03ed320cf6693d7cc9ac25d7667772d7034c9d86
thanhnam412 <nam2005mns08@gmail.com>
2025-03-15 23:12:10 +0700

    update

--- backend/app/app.py
+++ backend/app/app.py
@@ -9,3 +9,28 @@ def hello_world():
 @app.route('/user/<username>')
 def show_user_profile(username):
     return f'User {escape(username)}'
+
+# app = Flask(__name__)
+
+# def get_db_connection():
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port="5432",
+#         user="cps",
+#         password="password",
+#         database="database_name",
+#     )
+#     return conn
+
+# @app.route('/')
+# def index():
+#     conn = get_db_connection()
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM users;")
+#     users = cur.fetchall()
+#     cur.close()
+#     conn.close()
+#     return f"Danh sách người dùng: {users}"
+
+if __name__ == "__main__":
+    app.run( host="localhost", port="8080" )
